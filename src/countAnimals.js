fc9026d373e2f8901a795cc33864fcdca4049434
Luis <l-serrato@live.com>
2022-10-31 18:15:51 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const beast = data.species.find((specie) => specie.name === animal.specie);
+  if (animal.sex) {
+    return beast.residents.filter((bicho) => bicho.sex === animal.sex).length;
+  }
+  return beast.residents.length;
 }
 
 module.exports = countAnimals;
