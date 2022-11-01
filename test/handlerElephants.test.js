128dceca099f646455ed5c9487e6d5c931d4ca0d
Luis <l-serrato@live.com>
2022-10-31 22:05:11 -0300

    requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('counter test', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('names test', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('age average test', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('location test', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('popularity test', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('availability test', () => {
+    expect(handlerElephants('availability')).toContain('Sunday');
+  });
+  it('empty parameter test', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('invalid parameter test', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('random value test', () => {
+    expect(handlerElephants('random value')).toBeNull();
+  });
+});
