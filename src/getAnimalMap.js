8998552a8f50fc886d80ec6d958001c80e14c674
Luis <l-serrato@live.com>
2022-11-04 14:47:57 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,61 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const { species } = data;
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const getSpeciesLocation = () => {
+  const speciesLocations = locations.map((location) => (
+    {
+      [location]: species.filter((animal) => animal.location === location)
+        .map((animal) => animal.name),
+    }
+  ));
+  return speciesLocations.reduce((obj, item) => Object.assign(obj, item), {});
+};
+
+const getSpeciesBySex = (options) => {
+  const speciesBySex = locations.map((location) => {
+    const speciesFilter = species.filter((animal) => animal.location === location);
+    if (options.sex && options.sorted) {
+      return {
+        [location]: speciesFilter
+          .map((animal) => ({ [animal.name]: animal.residents
+            .filter((resident) => resident.sex === options.sex).map((anm) => anm.name).sort() })),
+      };
+    }
+    return {
+      [location]: speciesFilter
+        .map((animal) => ({ [animal.name]: animal.residents
+          .filter((resident) => resident.sex === options.sex).map((anm) => anm.name) })),
+    };
+  });
+  return speciesBySex.reduce((obj, item) => Object.assign(obj, item), {});
+};
+
+const getSpeciesWithNames = (options) => {
+  const speciesWithNames = locations.map((location) => {
+    const speciesFilter = species.filter((animal) => animal.location === location);
+    if (options.sex) {
+      return getSpeciesBySex(options);
+    }
+    if (options.sorted && !options.sex) {
+      return { [location]: speciesFilter
+        .map((animal) => ({ [animal.name]: animal.residents
+          .map((resident) => resident.name).sort() })) };
+    }
+    return { [location]: speciesFilter.map((animal) => ({ [animal.name]: animal.residents
+      .map((resident) => resident.name) })) };
+  });
+  return speciesWithNames.reduce((obj, item) => Object.assign(obj, item), {});
+};
+
+const getAnimalMap = (options) => {
+  if (!options || (options.sex && !options.includeNames)) {
+    return getSpeciesLocation();
+  }
+  if (options.includeNames) {
+    return getSpeciesWithNames(options);
+  }
+};
 
 module.exports = getAnimalMap;
